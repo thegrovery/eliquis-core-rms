bfe6b7ae74d45e25b97bb3033b50dba2d2559356
Alec Reimel <alecreimel1@gmail.com>
2023-10-05 22:03:08 -0400

    fixed PWA config for build

--- astro.config.ts
+++ astro.config.ts
@@ -61,12 +61,7 @@ export default defineConfig({
     workbox: {
       navigateFallback: '/offline',
       globDirectory: '/',
-  		globPatterns: [
-  			globPatterns: ["**/*"],
-  		],
-      includeAssets: [
-                "**/*",
-            ],
+  		globPatterns: ["**/*"],
       runtimeCaching: [
         {
           urlPattern: ({ url }) => {
