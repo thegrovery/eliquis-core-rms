6542379aef97c89a1f23de45bcfd7f030dfcb407
Alec Reimel <alecreimel1@gmail.com>
2023-10-26 15:29:19 -0400

    astro config tab formatting fixxed

--- astro.config.ts
+++ astro.config.ts
@@ -30,49 +30,49 @@ export default defineConfig({
   astroCodeSnippets(), 
   mdx(), 
   AstroPWA({
-        mode: 'development',
-        base: '/',
-        scope: '/',
-        includeAssets: ['favicon.svg'],
-        registerType: 'autoUpdate',
-        manifest: {
-          name: 'Eliqus Field Force Navigator',
-          short_name: 'FFN',
-          theme_color: '#76004B',
-          icons: [
-            {
-              src: 'pwa-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-            {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any maskable',
-            },
-          ],
+    mode: 'development',
+    base: '/',
+    scope: '/',
+    includeAssets: ['favicon.svg'],
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Eliqus Field Force Navigator',
+      short_name: 'FFN',
+      theme_color: '#76004B',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
         },
-        workbox: {
-          navigateFallback: '/offline',
-          globDirectory: 'dist',
-  		globPatterns: [
-  			'**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico,html}',
-  		],
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
         },
-        devOptions: {
-          enabled: true,
-          navigateFallbackAllowlist: [/^\/offline/],
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
         },
-      }),
-  	prefetch({
-  	      //prefetch options
-	}),
+      ],
+    },
+    workbox: {
+      navigateFallback: '/offline',
+      globDirectory: 'dist',
+      globPatterns: [
+      	'**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico,html}',
+      ],
+    },
+    devOptions: {
+      enabled: true,
+      navigateFallbackAllowlist: [/^\/offline/],
+    },
+  }),
+	prefetch({
+    //prefetch options
+	 }),
   ],
   markdown: {
     syntaxHighlight: 'shiki',
